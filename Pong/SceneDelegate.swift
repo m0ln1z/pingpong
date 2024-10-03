0612c94844414816af5fe40520ead85904cb246c
Artur <159512582+m0ln1z@users.noreply.github.com>
2024-10-03 22:53:15 +0500

    Update SceneDelegate.swift

--- Pong/SceneDelegate.swift
+++ Pong/SceneDelegate.swift
@@ -1,12 +1,10 @@
 import UIKit
 
-/// Этот класс обрабатывает состояния "сцен" отображения приложения
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    /// В этой функции мы могли бы настроить главное окно отображения приложения
-    /// Если бы решили верстать UI в коде, а не в Storyboard'е
+=
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
